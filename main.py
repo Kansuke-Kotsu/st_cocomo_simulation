138995dda95070c2db282c84b3102ad08dad8158
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2024-01-15 20:44:02 +0900

    update

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-# 3x10のデータを作成
-data = {'Column 1': [1, 2, 3],
-        'Column 2': [4, 5, 6],
-        'Column 3': [7, 8, 9],
-        'Column 4': [10, 11, 12],
-        'Column 5': [13, 14, 15],
-        'Column 6': [16, 17, 18],
-        'Column 7': [19, 20, 21],
-        'Column 8': [22, 23, 24],
-        'Column 9': [25, 26, 27],
-        'Column 10': [28, 29, 30]}
+def caliculation(initial_bed):
+        # 1列目
+        num = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+        # 2列目
+        bed = np.array([1,1,2,3,5,8,13,21,34,55,89,144,233,377,610])
+        # 3列目
+        total = np.array([1,2,4,7,12,20,33,54,88,143,232,376,609,986,1596])
+        # 4列目
+        benefit = initial_bed * (bed * 3 - total)
+        # 5列目
+        rate = []
+        for i in range (len(num)):
+                rate.append((2/3)**num[i])
+        # 表を描画
+        data = {
+                '回数': num,
+                '掛け金': bed * initial_bed,
+                'トータル': total * initial_bed,
+                '利益': benefit,
+                '負け確率': rate
+                }
+        df = pd.DataFrame(data)
+        return df
 
-df = pd.DataFrame(data)
 
-# Streamlitアプリケーションの作成
-st.title('3x10の表を描画する')
+def main():
+        # Streamlitアプリケーションの作成
+        st.title('ココモ法 シミュレーション')
+        initial_bed = st.number_input("input initial bet", step=500, value=0)
+        if  initial_bed:
+                df = caliculation(initial_bed=initial_bed)
+                st.table(df)
 
-# 表の描画
-st.table(df)
+if __name__ == '__main__':
+        main()
